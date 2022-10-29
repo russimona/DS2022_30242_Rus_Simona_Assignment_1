f2aa4d56aed687990488010be6bc0366139080b0
Simona Rus <Rus.Si.Simona@student.utcluj.ro>
2022-10-29 16:34:03 +0300

    finish ui for login & signup

--- react-demo-hooks/src/register/login/login.js
+++ react-demo-hooks/src/register/login/login.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FormGroup, Input, Label, Button } from "reactstrap";
 
 //others
@@ -6,26 +6,66 @@ import wallpaper from "../../assets/register_page.jpg";
 import "./login.css";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [alertMessage, setAlertMessage] = useState("");
+  const [isAlert, setIsAlert] = useState(false);
+  const loginHandler = () => {
+    validateInput();
+   // window.location.href = "/";
+  };
 
-const loginHandler =({String: email, String: password})=>{
-    window.location.href = '/';
-}
+  const handleChange = (event) => {
+    if (event.target.name === "email") {
+      setEmail(event.target.value);
+    }
+    if (event.target.name === "password") {
+      setPassword(event.target.value);
+    }
+  };
+
+
+  const validateInput = () => {
+    if (email.length < 10 || password.length < 5) {
+      setIsAlert(true);
+      setAlertMessage("Please enter a correct username and password. ");
+    } else {
+      setIsAlert(false);
+      setAlertMessage("");
+    }
+  };
 
   return (
     <div
       style={{ backgroundImage: `url(${wallpaper})` }}
       className="wallpaperImage"
     >
-     
-      <FormGroup className='formlogin'>
-      <h1 className="title">Login</h1>
-        <Label className='labelloginform' for="emailField"> Email </Label>
-        <Input  name="username" type="text" placeholder={"Email"} />
-        <Label  className='labelloginform'  for="passwordField" > Password </Label>
-        <Input name="username" type="text" placeholder={"Password"} />
-        <a href='/signup' className="neddAccount">Need an account? SIGN UP</a>
-        <Button className="submitbutton" onClick={loginHandler}>
-            Login
+      {isAlert && <div className="alertText">{alertMessage}</div>}
+      <FormGroup className="formlogin">
+        <h1 className="title">Login</h1>
+        <Label className="labelloginform" for="emailField">
+          Email
+        </Label>
+        <Input
+          name="email"
+          type="text"
+          placeholder={"Email"}
+          onChange={handleChange}
+        />
+        <Label className="labelloginform" for="passwordField">
+          Password
+        </Label>
+        <Input
+          name="password"
+          type="text"
+          placeholder={"Password"}
+          onChange={handleChange}
+        />
+        <a href="/signup" className="neddAccount">
+          Need an account? SIGN UP
+        </a>
+        <Button color="info" className="submitbutton" onClick={loginHandler}>
+          Login
         </Button>
       </FormGroup>
     </div>
--- react-demo-hooks/src/register/signup/signup.js
+++ react-demo-hooks/src/register/signup/signup.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FormGroup, Input, Label, Button } from "reactstrap";
 
 //others
@@ -6,8 +6,67 @@ import wallpaper from "../../assets/register_page.jpg";
 import "./signup.css";
 
 const SignUp = () => {
-  const signupHandler = ({ String: email, String: password }) => {
-    window.location.href = "/login";
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password1, setPassword1] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [alertMessage, setAlertMessage] = useState("");
+  const [isAlert, setIsAlert] = useState(false);
+
+  const signupHandler = () => {
+    validateInput();
+   // window.location.href = "/login";
+  };
+
+  const handleChange = (event) => {
+    if (event.target.name === "firstname") {
+      setFirstName(event.target.value);
+    }
+    if (event.target.name === "lastname") {
+      setLastName(event.target.value);
+    }
+    if (event.target.name === "email") {
+      setEmail(event.target.value);
+    }
+    if (event.target.name === "password1") {
+      setPassword1(event.target.value);
+    }
+    if (event.target.name === "password2") {
+      setPassword2(event.target.value);
+    }
+
+  };
+
+  const validateInput = () => {
+    if (firstName.length < 2) {
+      setIsAlert(true);
+      setAlertMessage("Please enter a valid first name");
+    } else {
+      setIsAlert(false);
+      setAlertMessage("");
+    }
+    if (lastName.length < 2) {
+      setIsAlert(true);
+      setAlertMessage("Please enter a valid last name");
+    } else {
+      setIsAlert(false);
+      setAlertMessage("");
+    }
+    if (email.length < 10) {
+      setIsAlert(true);
+      setAlertMessage("Please enter a valid email");
+    } else {
+      setIsAlert(false);
+      setAlertMessage("");
+    }
+    if (password1 !== password2 && password1.length < 5) {
+      setIsAlert(true);
+      setAlertMessage("Passwords do not match");
+    } else {
+      setIsAlert(false);
+      setAlertMessage("");
+    }
   };
 
   return (
@@ -15,34 +74,55 @@ const SignUp = () => {
       style={{ backgroundImage: `url(${wallpaper})` }}
       className="wallpaperImage"
     >
+      {isAlert && <div className="alertText">{alertMessage}</div>}
       <FormGroup className="formsignup">
         <h2 className="title">Create an account</h2>
         <Label className="labelsignupform" for="emailField">
-          {" "}
-          First name{" "}
+          First name
         </Label>
-        <Input name="username" type="text" placeholder={"First name"} />
+        <Input
+          name="firstname"
+          type="text"
+          placeholder={"First name"}
+          onChange={handleChange}
+        />
         <Label className="labelsignupform" for="emailField">
-          {" "}
-          Last name{" "}
+          Last name
         </Label>
-        <Input name="username" type="text" placeholder={"Last name"} />
+        <Input
+          name="lastname"
+          type="text"
+          placeholder={"Last name"}
+          onChange={handleChange}
+        />
         <Label className="labelsignupform" for="emailField">
-          {" "}
-          Email{" "}
+          Email
         </Label>
-        <Input name="username" type="text" placeholder={"Email"} />
+        <Input
+          name="email"
+          type="text"
+          placeholder={"Email"}
+          onChange={handleChange}
+        />
         <Label className="labelsignupform" for="passwordField">
-          {" "}
-          Password{" "}
+          Password
         </Label>
-        <Input name="username" type="text" placeholder={"Password"} />
+        <Input
+          name="password1"
+          type="text"
+          placeholder={"Password"}
+          onChange={handleChange}
+        />
         <Label className="labelsignupform" for="passwordField">
-          {" "}
-          Repeat password{" "}
+          Repeat password
         </Label>
-        <Input name="username" type="text" placeholder={" Repeat password"} />
-        <Button className="submitbutton" onClick={signupHandler}>
+        <Input
+          name="password2"
+          type="text"
+          placeholder={" Repeat password"}
+          onChange={handleChange}
+        />
+        <Button color="info" className="submitbutton" onClick={signupHandler}>
           Sign up
         </Button>
       </FormGroup>
